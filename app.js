b478d933ec2bb13e9876ad00d24761fcea54fcc2
Parminder Singh <parmsingh129@gmail.com>
2018-09-18 11:26:14 +0530

    Added backend

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const accountSid = '';
+const authToken = '';
+const client = require('twilio')(accountSid, authToken);
+
+app.post('/sms', (req, res) => {
+    console.log(req.body);
+    client.messages.create({
+        body: req.body.content,
+        from: '+17062227746',
+        to: '+919417763589'
+    }).then(_ => {
+        res.end('Done');
+    }).catch(err => console.log);
+});
+
+const listener = app.listen(process.env.PORT || 8080, function(){
+    console.log(`Listening at : ${process.env.PORT || 8080}`);
+});
